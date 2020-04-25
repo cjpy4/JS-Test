dffc9119016189a540c6db9ef325589f68db4551
jpye4 <jimmycliff4@gmail.com>
2020-04-24 23:17:18 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,3 @@
+const log = require("./log");
+
+console.log(log());
---
+++ log.js
@@ -0,0 +1,7 @@
+var url = "http://mylogger.org/log";
+var message = "hello";
+function log() {
+  console.log(message);
+}
+
+module.exports = log;
