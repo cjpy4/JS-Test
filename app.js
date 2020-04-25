2c05ad225ebe6f53ef6c8ae360f034f06f535fd3
jpye4 <jimmycliff4@gmail.com>
2020-04-24 23:41:47 -0500

    Added Functions

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 const log = require("./log");
+const os = require("os");
+var totalMemory = os.totalmem;
+var freeMemory = os.freemem;
 
 console.log(log());
+console.log(`Total Memory: ${totalMemory}`);
+console.log(`Free Memory: ${freeMemory}`);
